2876c1b4a863d73449b785b2b0d2593d4894fdd3
Ingoo Kang <47412229+kig2929kig@users.noreply.github.com>
2024-10-21 12:17:21 +0900

    Add files via upload

--- snakeGame.py
+++ snakeGame.py
@@ -1,7 +1,18 @@
 import pygame
+import time
+
+def message(fonts, msg, color, posx, posy):
+    mesg = fonts.render(msg, True, color)
+    mesg_Rect = mesg.get_rect()
+    mesg_Rect.centerx = posx
+    mesg_Rect.centery = posy
+    screen.blit(mesg, mesg_Rect)
 
 pygame.init()
 clock = pygame.time.Clock()
+#Fonts
+font_gameOver = pygame.font.SysFont(None, 50)
+font_madeBy = pygame.font.SysFont(None, 20)
 
 #color
 RED = (255,0,0); GREEN = (0,255,0)
@@ -40,15 +51,27 @@ while running:
             if event.key == pygame.K_RIGHT:
                 snake_posx_change = 10
                 snake_posy_change = 0
+    if snake_pos_x >= (SCR_WIDTH - SNAKE_SIZE) or \
+       snake_pos_x - (SNAKE_SIZE/2) < 0 or \
+       snake_pos_y >= (SCR_HEIGHT - SNAKE_SIZE) or \
+       snake_pos_y - (SNAKE_SIZE/2) < 0 :
+        running = False       
                 
     snake_pos_x += snake_posx_change
     snake_pos_y += snake_posy_change
     screen.fill(WHITE)
+
+    pygame.draw.rect(screen, BLACK, [0,0, SCR_WIDTH, SCR_HEIGHT],10)
     pygame.draw.rect(screen, RED, [snake_pos_x,
                             snake_pos_y,
                             SNAKE_SIZE,
                             SNAKE_SIZE])
+
     
     pygame.display.update()
     clock.tick(60)
+message(font_gameOver, 'Game Over', RED, int(SCR_WIDTH/2), int(SCR_HEIGHT/2))
+message(font_madeBy, 'made by kig2929kig', BLACK, int(SCR_WIDTH/2), int(SCR_HEIGHT/2)+30)
+pygame.display.update()
+time.sleep(2)
 pygame.quit()
